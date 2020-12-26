ad537ada76a7bafb0aea311073d86dcd1026a1d7
LM52114 <15680623309@163.com>
2020-12-26 23:23:49 +0800

    test

---
+++ Online/src/main/java/HttpClient/HttpGetTest.java
@@ -0,0 +1,9 @@
+package Online.src.main.java.HttpClient;
+
+/**
+ * @Author Li Hao
+ * @Date 2020/12/26 23:21
+ * @Version 1.0
+ */
+public class HttpGetTest {
+}
---
+++ Online/src/main/java/HttpClient/HttpPostParameterTest.java
@@ -0,0 +1,9 @@
+package Online.src.main.java.HttpClient;
+
+/**
+ * @Author Li Hao
+ * @Date 2020/12/26 23:20
+ * @Version 1.0
+ */
+public class HttpPostParameterTest {
+}
