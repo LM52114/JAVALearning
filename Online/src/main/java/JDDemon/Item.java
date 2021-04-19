c432c1c0b9f67d99a119ef8242fa10254687647c
Administrator <asd>
2021-04-19 20:06:30 +0800

    默认更改列表

---
+++ Online/src/main/java/JDDemon/Item.java
@@ -0,0 +1,14 @@
+package JDDemon;
+
+import com.sleepycat.persist.model.Entity;
+
+import javax.annotation.concurrent.Immutable;
+
+/**
+ * @Author Li Hao
+ * @Date 2020/12/27 21:01
+ * @Version 1.0
+ */
+@Entity
+public class Item {
+}
